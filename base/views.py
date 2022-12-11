c82295d5e33d3c8cc4edf51d57c1935dc3341d5b
calin <sanducalinm@gmail.com>
2022-12-11 18:37:14 +0200

    pagination v1.0

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Product, Category
 
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 
@@ -11,12 +13,22 @@ def homePage(request):
 def productsPage(request):
     category = request.GET.get('category')
     if category == None:
-        products = Product.objects.all()[0:3]
+        # products = Product.objects.all()[0:3]
+        p = Paginator(Product.objects.all(), 2)
+        page = request.GET.get('page')
+        products_p = p.get_page(page)
+        count = Product.objects.all().count()
+        page = 'toate'
     else:
-        products = Product.objects.filter(category__name__icontains=category)
+        # products = Product.objects.filter(category__name__icontains=category)
+        p = Paginator(Product.objects.filter(category__name__icontains=category), 2)
+        page = request.GET.get('page')
+        products_p = p.get_page(page)
+        count = Product.objects.filter(category__name__icontains=category).count()
+        page = 'category'
 
     categories = Category.objects.all()
-    context = {'products': products, 'categories': categories}
+    context = {'categories': categories, 'products_p': products_p, 'count': count, 'page': page}
     return render(request, 'base/products.html', context)
 
 
